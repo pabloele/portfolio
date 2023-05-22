21791503e8361b26d105a5e9b7204ae9c9e98fb6
Pablo Levy <114173733+pabloele@users.noreply.github.com>
2023-05-22 09:55:52 -0300

    next-fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import "@/styles/globals.css";
 import Navbar from "../components/navbar";
+import { Bruno_Ace } from "@next/font/google";
+
+const brunoAce = Bruno_Ace({
+  subsets: [],
+  weight: "400",
+});
+
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <div className={brunoAce.className}>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
