2403c26475cbc7b009ad3451a08a2829f7a63966
Pablo Levy <114173733+pabloele@users.noreply.github.com>
2023-07-19 19:53:33 -0300

    fix openvino card

--- components/proyects.jsx
+++ components/proyects.jsx
@@ -49,7 +49,7 @@ export default function Proyects() {
                 Openvino Dashboard
               </h3>
               <p className="pb-4 pt-2 text-white text-center">
-                Next.JS-Postgresql <br /> Metamask-Tailwind
+                Next-Postgresql <br /> Metamask-Tailwind
               </p>
               <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
                 <Link href="/openvino">Ver más</Link>
@@ -86,7 +86,7 @@ export default function Proyects() {
             />
             <div className="hidden group-hover:block absolute  left-[50%] translate-x-[-50%]">
               <h3 className="text-2xl text-white tracking-wider text-center">
-                Netflix - Clone
+                Netflix Clone
               </h3>
               <p className="pb-4 pt-2 text-white text-center">
                 React - Tailwind - Firebase - Firestore
